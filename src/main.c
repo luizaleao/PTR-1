ade7ca640952023744c034882ecc0f34dd77e90c
RodrigoPrintes <drigoprintes00@gmail.com>
2022-07-14 10:48:14 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -77,12 +77,9 @@ int main(int argc, char **argv) {
     FX_ *ptrPow = potencia;
 
 
-    double s = 0 ;
-    for( float i  = 1; i < 5 ; i+= 1){
-         s+= integral_trapezioSimples(i, i+1,ptrPow );
-    }
+   
     double s2 = integral_Trapezio( 0,  5 ,  ptrPow);
-    printf("%f %.1f" , s,s2);
+    printf("%f " ,s2);
 
     return 0 ;
 
